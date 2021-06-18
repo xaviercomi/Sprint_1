8468caa40114d21b08f66c125e69d8ffed978ad1
unknown <formacio@PC-1062627.formacio.local>
2021-06-18 12:58:39 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let age = prompt('What is your age?', 18);
+
+let welcome;
+
+if (age < 18) {
+    welcome = function() {
+        alert('Hello!');
+    };
+
+} else {
+    welcome = function() {
+        alert('Greetings!');
+    };
+}
+
+    welcome();
