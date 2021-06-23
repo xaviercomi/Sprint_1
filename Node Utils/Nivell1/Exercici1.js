55164983a6772b3e6b53f4eefa1f123b6f77678f
xavier comi <xaviercomi@gmail.com>
2021-06-23 17:40:30 +0200

    last_version

--- Node Utils/Nivell1/Exercici1.js
+++ Node Utils/Nivell1/Exercici1.js
@@ -2,28 +2,24 @@ let contador = 0;
 let respuesta;
 let interval;
 
+interval = setInterval(missatgeDemora, 1000);
+
 function missatgeDemora() {
 
     console.log(`${contador} Hola!`)
     contador++;
 
     if (contador % 10 == 0) {
-        stopInterval();
-
-        
-        process.stdout.write('¿Quieres continuar? (s/n): \n');
-
-        process.stdin.on('data', function(data) { 
-            respuesta = data.toString().trim()
-            if ( respuesta == 's')  {
-                process.exit();
-            }  
-        });
+        stopInterval();     
     }        
 }
 
-interval = setInterval(missatgeDemora, 1000);
-
 function stopInterval(){
-    clearInterval(interval);
+    process.stdout.write('¿Quieres continuar? (s/n): \n');    
+    process.stdin.on('data', function(data) { 
+        respuesta = data.toString().trim()
+        if ( respuesta == 's')  {
+            process.exit();
+        }  
+    });
 }
