c9da4a9466a6ceff596dc854624f3c17b47d51fa
xavier comi <xaviercomi@gmail.com>
2021-06-25 14:01:45 +0200

    Array de funciones no funciona

--- Functions & Templates Literals/Nivell3/Exercici1.js
+++ Functions & Templates Literals/Nivell3/Exercici1.js
@@ -1,14 +1,28 @@
-let funcions = Array(10);
-let i = 0;
-let j = 0;
 
-for(i=0; i < funcions.length; i++) {
+let funcions = new Array(10);
+let matriuFuncions = new Array(10);
+
+for ( let k = 0; k < matriuFuncions; k++) {
+    matriuFuncions[k] = function compta() {
+        while ( k < matriuFuncions.length ) {
+            return console.log ( k + '\n');
+        }    
+    };
+    console.log( matriuFuncions.forEach(element => element.compta() ));
+    console.log ( matriuFuncions );
+}
+
+for( let i = 0; i < funcions.length; i++ ) {
     funcions[i] = `funcio_${i}`;
 }
 
 funcions.forEach((element, index) => {
         console.log(`A la posicio ${index} hi ha l'element ${element} que compta de zero a nou`);
-        for(j=0; j < funcions.length; j++) {
+        for( let j = 0; j < funcions.length; j++ ) {
             console.log([j]+'\n');
         }
 });
+
+
+
+
