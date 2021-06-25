745ebb9a5cfb645dbf9925a1a5a7dbe2ffe225b1
Xavier Comi <xaviercomi@gmail.com>
2021-06-25 02:32:51 +0200

    array_funcions

--- Functions & Templates Literals/Nivell3/Exercici1.js
+++ Functions & Templates Literals/Nivell3/Exercici1.js
@@ -1,2 +1,14 @@
-let sum = (a, b) => a +b;
-console.log(sum(3, 5));
+let funcions = Array(10);
+let i = 0;
+let j = 0;
+
+for(i=0; i < funcions.length; i++) {
+    funcions[i] = `funcio_${i}`;
+}
+
+funcions.forEach((element, index) => {
+        console.log(`A la posicio ${index} hi ha l'element ${element} que compta de zero a nou`);
+        for(j=0; j < funcions.length; j++) {
+            console.log([j]+'\n');
+        }
+});
