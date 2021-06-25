22a3009f3c32b2a2cd8715f1f8303f7c7287e147
xavier comi <xaviercomi@gmail.com>
2021-06-25 23:13:53 +0200

    arrow function amb parametre que torna un objecte.

---
+++ Classes & Arrow Functions/Nivell1/Exercici1.js
@@ -0,0 +1,2 @@
+let sumaNombres = (num1, num2) => num1 + num2;
+console.log( sumaNombres(68, 57) );
---
+++ Classes & Arrow Functions/Nivell2/Exercici1.js
@@ -0,0 +1,8 @@
+let client = (nom, edat, pes) => ({
+     nom: nom,
+     edat: edat,
+     pes: pes
+});
+
+console.log(client('Xavier', 38, 75));
+
