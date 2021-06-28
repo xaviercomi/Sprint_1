f422f414cd2e3ac3fce34eaa8daf2837fd2b9606
xavier comi <xaviercomi@gmail.com>
2021-06-28 12:27:41 +0200

    Classe i arrows functions finalitzat

--- Classes & Arrow Functions/Nivell3/Exercici1.js
+++ Classes & Arrow Functions/Nivell3/Exercici1.js
@@ -1,7 +1,13 @@
 
 
-function movil (marca, pantalla, color) {
+function auto (marca, matricula) {
     this.marca = marca,
-    this.pantalla = pantalla,
-    this.color = color
-    };
+    this.matricula = matricula
+}
+let auto1 = new auto('Seat','345SFG');
+let auto2 = new auto('Skoda','567TRY');
+let auto3 = new auto('Volkswagen','789XCV')
+
+console.log('\nMarca: ' + auto1.marca, '\nMatricula: ' + auto1.matricula);
+console.log('\nMarca: ' + auto2.marca, '\nMatricula: ' + auto2.matricula);
+console.log('\nMarca: ' + auto3.marca, '\nMatricula: ' + auto3.matricula);
