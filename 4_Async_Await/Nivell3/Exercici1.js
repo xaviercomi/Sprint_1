2ccf516ff0f4ef4e9ae631afe340e2d5116dfefa
Xavier Comi <xaviercomi@gmail.com>
2021-07-02 09:38:22 +0200

    version_entrega

--- 4_Async_Await/Nivell3/Exercici1.js
+++ 4_Async_Await/Nivell3/Exercici1.js
@@ -1,9 +1,24 @@
-async function getName() {
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve('Xavier'), 2000)
+
+
+function getName(nom) {
+    return new Promise((resolve, reject) => {
+        if(nom != 'Xavier') {
+            reject(new Error(`Nom: ${nom}. No vàlid`))
+        }
+        setTimeout(() => resolve(`Nom: ${nom}. vàlid`), 2000)
     });
-    let result = await promise; 
-    console.log(result);
 }
 
-getName();
+async function repNoms (nom) {
+    try {
+    const names = await getName(nom);
+    console.log(names);
+    } catch (err) {
+      console.log(err.message);
+    }
+};
+
+repNoms('Lucas');
+
+
+setTimeout(() => repNoms('Xavier'), 2500);
