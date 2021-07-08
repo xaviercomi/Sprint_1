fe8131487740890efdcc443ea481d4027243d7a3
xavier comi <xaviercomi@gmail.com>
2021-07-08 07:42:30 +0200

    Classes & Arrow modificat

--- 2_Classes & Arrow Functions/Nivell1/Exercici1.js
+++ 2_Classes & Arrow Functions/Nivell1/Exercici1.js
@@ -1,2 +1,4 @@
-let sumaNombres = (num1, num2) => num1 + num2;
-console.log( sumaNombres(68, 57) );
+((num1, num2) => {
+    let suma = num1 + num2;
+    console.log(`${num1} + ${num2} = ${suma}`);
+})(2, 2);
--- 2_Classes & Arrow Functions/Nivell3/Exercici1.js
+++ 2_Classes & Arrow Functions/Nivell3/Exercici1.js
@@ -1,13 +1,25 @@
+let stockVehiculos = [10];
 
 
-function auto (marca, matricula) {
-    this.marca = marca,
-    this.matricula = matricula
+class Vehiculo {
+    constructor(tipo, marca, matricula) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.matricula = matricula;
+    }
 }
-let auto1 = new auto('Seat','345SFG');
-let auto2 = new auto('Skoda','567TRY');
-let auto3 = new auto('Volkswagen','789XCV')
 
-console.log('\nMarca: ' + auto1.marca, '\nMatricula: ' + auto1.matricula);
-console.log('\nMarca: ' + auto2.marca, '\nMatricula: ' + auto2.matricula);
-console.log('\nMarca: ' + auto3.marca, '\nMatricula: ' + auto3.matricula);
+function creaVehicle(tipo, marca, matricula) {
+     
+    let vehiculo = new Vehiculo(tipo, marca, matricula);
+    stockVehiculos.push(vehiculo);
+    console.log(stockVehiculos);
+
+}
+
+creaVehicle('moto', 'Seat','345SFG');
+creaVehicle('triciclo', 'Skoda','567TRY');
+creaVehicle('automovil', 'Volkswagen','789XCV');
+
+
+
