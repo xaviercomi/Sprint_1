c11b5adcf3fa787f4b387e810cfdb6b05567bd9e
Xavier Comi <xaviercomi@gmail.com>
2021-07-21 13:15:18 +0200

    Promises_Callback: entregar

--- 3_Promises_Callbacks/Nivell3/Exercici1.js
+++ 3_Promises_Callbacks/Nivell3/Exercici1.js
@@ -5,60 +5,57 @@ let employees = [{
 }, {
     id: 2,
     name: 'Bill Gates'
-},{
+}, {
     id: 3,
     name: 'Jeff Bezos'
 }];
- 
+
 let salaries = [{
     id: 1,
     salary: 4000
 }, {
     id: 2,
-    salary: 1000    
-}, {
-    id: 3,
-    salary: 2000
+    salary: 1000
 }];
 
 
-const getEmpleado = ( id => {
-        
-    return new Promise ( (resolve, reject) => {  
+const getEmpleado = (id => {
+
+    return new Promise((resolve, reject) => {
         let employee = employees.find(item => item.id == id);
-            if (employee) {
-                resolve(employee);
-            } else {
-                reject(err = new Error('empleat no trobat'));
-            }   
+        if (employee) {
+            resolve(employee);
+        } else {
+            reject(err = new Error('empleat no trobat'));
+        }
     });
 
 });
 
 
-let getSalario = ( employee => {
-    
+let getSalario = (employee => {
 
-    return new Promise( (resolve, reject) => {
+
+    return new Promise((resolve, reject) => {
         let salari = salaries.find(item => item.id == employee.id);
         if (salari) {
             resolve(salari);
         } else {
+
             reject(err1 = new Error('salari no trobat'));
-        }    
-    });  
+        }
+    });
 
 });
 
-try {
-    getEmpleado(3)
-        .then( employee => {
-            console.log(`ID[${employee.id}] pertany a: ${employee.name}`)
-            return getSalario(employee) 
-        .then( salari => console.log(`Usuari ID[${salari.id}] salari: ${salari.salary}`) ) 
-        .catch( (err) => console.log(err) ) 
-        .catch( (err1) => console.log(err1) )
-        });
-} catch (error) {
-    console.log(`Funció no trobada ${error}`);
-}
+
+getEmpleado(3)
+    .then(employee => {
+        console.log(`ID[${employee.id}] pertany a: ${employee.name}`)
+
+        getSalario(employee)
+            .then(salari => console.log(`Usuari ID[${salari.id}] salari: ${salari.salary}`))
+            .catch((err) => console.log("ERROR SALARIO: " + err));
+
+    })
+    .catch((err1) => console.log("ERROR EMPOLEADO: " + err1));
